589d2a5c754a5a5f66151286d25849b9a942b904
Emilio Zamorano <72397846+emiliozamorano15@users.noreply.github.com>
2021-04-26 18:05:34 +0200

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,16 +1,97 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet'])
+import sqlite3
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
 
 def load_data(database_filepath):
-    pass
-
+    '''
+    Read data from local database
+    INPUT:
+        database_filepath: str path to local database file
+    OUTPUT:
+        table pandas dataframe
+    '''
+    conn = sqlite3.connect(database_filepath)
+    
+    df = pd.read_sql("SELECT * FROM tbl_disaster_response", conn)
+    
+    X = df.message.values
+    df_Y = df.drop(['index', 'id', 'message', 'original', 'genre'], axis = 1)
+    category_names = df_Y.columns
+    Y = df_Y.values
+    
+    return X, Y, category_names
+ 
 
 def tokenize(text):
-    pass
+    '''
+    Obtain tokens from text. Transformations: to lower case, remove special characters,
+    remove urls, and remove trailing and leading spaces. The clean text is then tokenized 
+    by word and lemmatized.
+    INPUTS:
+        text: str string to be tokenized
+    
+    OUTPUTS: lst list of lemmatized tokens
+    '''
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text) 
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+    
 
 
 def build_model():
-    pass
+    ''' 
+    Pipeline and grid definition for model building
+    INPUTS:
+    OUTPUTS: a GridSearchCV object from scikitlearn
+        
+    '''
+    pipeline = Pipeline([
+        ('text_features',
+            Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])
+        ),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    parameters = {
+        'text_features__vect__ngram_range': ((1, 1), (1, 2), (2, 2)),
+        'text_features__vect__max_df': (0.5, 0.75, 1.0),
+        'text_features__vect_max_features': (None, 5000, 10000),
+        'text_features__tfidf_use_idf': (True, False),
+        'clf__n_estimators': [50, 100, 200],
+        'clf__min_samples_split': [2, 5, 10]
+    }
+    
+    cv = GridSearchCV(pipeline, param_grid=parameters, scoring = 'roc_auc')
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
