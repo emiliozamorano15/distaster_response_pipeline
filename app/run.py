e78d8de4265a1a0d26e1c8fe94cb457dfc3c7a02
Emilio Zamorano <72397846+emiliozamorano15@users.noreply.github.com>
2021-05-04 17:44:36 +0200

    fix: webapp running
    fixed wrong package imports, added plots

--- app/run.py
+++ app/run.py
@@ -1,5 +1,6 @@
 import json
 import plotly
+import numpy as np
 import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
@@ -7,8 +8,11 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+from plotly.graph_objs import Bar, Pie
+from plotly.express import colors
+
+# from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
@@ -26,11 +30,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('tbl_disaster_response', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -39,37 +43,48 @@ model = joblib.load("../models/your_model_name.pkl")
 def index():
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
-    graphs = [
-        {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
-            ],
-
-            'layout': {
-                'title': 'Distribution of Message Genres',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Genre"
-                }
-            }
-        }
-    ]
+    categories = df.drop(['original', 'index', 'id', 'message', 'genre'], axis = 1)
+    categories_count = categories.sum().sort_values().values
+    categories_names = categories.sum().sort_values().index
     
+    ## pie chart with counts per genre
+
+    trace1 = Pie(
+        labels = genre_names
+        , values = genre_counts
+        , marker_colors = colors.qualitative.Alphabet_r
+    )
+    
+    ## horizontal barchart with counts per category
+
+    trace2 = Bar(
+        x = categories_count
+        , y = categories_names
+        , orientation = 'h')
+    
+    ## append both graphs
+    graphs = [
+        {'data': [trace1],
+        'layout':{
+            'title':'Distribution of Message Genres'
+        }
+        },
+        {'data': [trace2],
+        'layout':{
+            'title': 'Distribution of Categories',
+            'yaxix':{'title':'Count'},
+            'xaxis':{'title':'Category'}
+        }
+        }
+            ]
+        
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -82,7 +97,7 @@ def go():
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
-    classification_results = dict(zip(df.columns[4:], classification_labels))
+    classification_results = dict(zip(df.columns[5:], classification_labels))
 
     # This will render the go.html Please see that file. 
     return render_template(
